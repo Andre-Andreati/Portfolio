a75560cdcf3b6c5de4fba3e7ea2ec1d1adef0709
Andre-Andreati <andre.andreati@gmail.com>
2025-04-30 20:25:43 -0300

    test theme toggle

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
-console.log('Hello')
-$(document).ready(function() {
-  //$("body").addClass("dark-golden")
-})
+if(window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+  theme = 'dark'
+} else {
+  theme = 'light'
+}
+
+const html = document.querySelector('html');
+var themeToggle = document.querySelector('#theme-toggle');
+
+console.log(theme)
+
+if (theme == 'dark') {
+  themeToggle.checked = true
+}
+
+themeToggle.addEventListener('click', function() {
+    if (themeToggle.checked) {
+        html.style.setProperty("color-scheme", "light")
+    } else {
+        html.style.setProperty("color-scheme", "dark")
+    }
+});
